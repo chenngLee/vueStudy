b2e9dddfd7b91abb87c5b0794fffac92f26f8d96
chenngLee <licheng736560926@outlook.com>
2020-01-17 22:27:30 +0800

    提交

---
+++ VuexTest/src/util/storageUtil.js
@@ -0,0 +1,17 @@
+/*
+使用localStorage 存储数据的工具模块
+向外暴露：
+1. 函数
+2. 对象
+ */
+const TODOS_KEY = 'todos_key';
+export default {
+  saveTodos (todos) {
+    window.localStorage.setItem(TODOS_KEY, JSON.stringify(todos))
+  },
+
+  readTodos () {
+    return JSON.parse(window.localStorage.getItem(TODOS_KEY) || '[]' )
+  }
+}
+
